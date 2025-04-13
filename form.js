a404e857dc242c3b5cc6b1ec8805a77f9621d854
Raju Pusti <rajupusti@yahoo.com>
2025-04-13 14:42:04 +0000

    form update

--- form.js
+++ form.js
@@ -1,5 +1,5 @@
+
 const form = document.getElementById("myForm");
-// const successDiv = document.getElementById("successMessage");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -9,35 +9,25 @@ form.addEventListener("submit", function (e) {
     email: form.email.value,
     message: form.message.value,
   };
-  console.log(JSON.stringify(formData));
-  
 
-  fetch(
-    "https://script.google.com/macros/s/AKfycbzY6m9zF6-LwiKJRdm2ZglfKowUUDqLIG2cc-FQPz30r2nT-7JUTtkNre1rDpeDRiuKxg/exec",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-      mode: "cors",
+  console.log(JSON.stringify(formData));
+
+  fetch("https://script.google.com/macros/s/AKfycbzICB1REw8whxHQRvJgD21ihxkdhLK-NY-Cu-7RcDg8BYJEA0uDQ0bhZyJkW8om-F45Ww/exec", {
+    method: "POST",
+    body: JSON.stringify(formData),
+    headers: {
+      "Content-Type": "application/json"
     }
-  )
-  
-    .then((res) => res.json())
-    .then((data) => {
-     
-        alert("Message sent successfully!");
-        // form.reset();
-        // successDiv.style.display = "block";
-        // successDiv.style.color = "green";
-        // successDiv.textContent =
-        //   "Thank you! Your message has been sent successfully.";
-     
-    });
-    // .catch((err) => {
-    //   successDiv.style.display = "block";
-    //   successDiv.style.color = "red";
-    //   successDiv.textContent = "Something went wrong. Please try again.";
-    // });
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log("Success:", data);
+    alert("Message sent successfully!");
+    form.reset();
+  })
+  .catch(error => {
+    console.error("Error:", error);
+    alert("Something went wrong. Please try again.");
+  });
 });
+
