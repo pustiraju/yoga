db9c4f5a288b7e2004eaa8f542d990e1009471d9
Raju Pusti <rajupusti@yahoo.com>
2025-05-17 07:21:45 +0000

    google sheet

--- center.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('registrationForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  fetch(this.action, { method: 'POST', body: new FormData(this) })
-    .then(response => response.text())
-    .then(result => alert(result));
-});
